d3179fc61c3ab10ed86453f58579a34e9335eccb
Jacobs12 <1440913385@qq.com>
2019-08-08 13:16:20 +0800

    1.0.0

---
+++ BJNewsTextToSpeech/BJNewsTextToSpeech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BJNewsTextToSpeech
+//
+//  Created by wolffy on 2019/8/8.
+//  Copyright © 2019 新京报社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
